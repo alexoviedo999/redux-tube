aa3cc381c7e9afaf4a5f12cc3188bbe790e9cf04
alexoviedo <alexoviedo999@gmail.com>
2016-01-19 00:34:36 -0500

    rendering thumbnail images from youtube api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyA0cgxVcTFlPtruOGs28r_z9Se8YkYhwL0';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			console.log(videos)
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
