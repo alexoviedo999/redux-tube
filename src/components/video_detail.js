a1e217edd73cc51acbd1ed976ddafd132696871e
alexoviedo <alexoviedo999@gmail.com>
2016-02-06 22:18:06 -0500

    video selection enabled

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -4,20 +4,19 @@ const VideoDetail = ({video}) => {
 	if (!video) {
 		return <div>Loading...</div>;
 	}
-	console.log(video)
+	console.log('snippit',video)
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;
 
 	return (
 		<div className="video-detail col-md-8">
+
 			<div className="embed-responsive embed-responsive-16by9">
 				<iframe className="embed-responsive-item" src={url}></iframe>
 			</div>
 			<div className="details">
-				{/*  why is the this not working?
-				<div>{video.snippit.title}</div>
-				<div>{video.snippit.description}</div>
-				*/}
+				<div>{video.snippet.title}</div>
+				<div>{video.snippet.description}</div>
 			</div>
 		</div>
 	);
